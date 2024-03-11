21413443727f334bd341cbd9f402544c69c4b73c
Wen Zhang <zhangwen@cs.berkeley.edu>
2024-03-10 21:02:06 -0700

    rails query attribute

--- spec/controllers/invoke_all_controller_spec.rb
+++ spec/controllers/invoke_all_controller_spec.rb
@@ -1,16 +1,58 @@
 # frozen_string_literal: true
 require 'jruby/dse/extensions/equality'
+require "jruby/dse/transcript_proto/expr_pb"
 require 'database_cleaner/active_record'
 
 # ActiveRecord::Base.logger = Logger.new(STDOUT)
 # ActiveRecord::Base.logger.level = Logger::DEBUG
 
+# FIXME(zhangwen): put these somewhere else?
 module ActiveSupport
   class TimeWithZone
     include Dse::SymbolicEquality
 
     def with_sym_ast(ast)
       dup.set_sym_ast ast
+    end
+  end
+end
+
+module ActiveRecord
+  module AttributeMethods
+    module Query
+      def query_attribute(attr_name)
+        value = self[attr_name]
+        res = Dse::Recorder.ignore do
+          case value
+          when true        then true
+          when false, nil  then false
+          else
+            column = self.class.columns_hash[attr_name]
+            if column.nil?
+              if Numeric === value || value !~ /[^0-9]/
+                !value.to_i.zero?
+              else
+                return false if ActiveModel::Type::Boolean::FALSE_VALUES.include?(value)
+                !value.blank?
+              end
+            elsif value.respond_to?(:zero?)
+              !value.zero?
+            else
+              !value.blank?
+            end
+          end
+        end
+
+        if value.symbolic?
+          res = res.with_sym_ast Dse::TranscriptProto::Expression.new(
+            call: Dse::TranscriptProto::Call.new(
+              function: "RAILS_QUERY_ATTRIBUTE",
+              arguments: [value.sym_ast]
+            )
+          )
+        end
+        res
+      end
     end
   end
 end
